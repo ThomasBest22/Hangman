3d889791b0d9f1e35a0292ac91b67964575a5c2c
ThomasBest22 <76630623+ThomasBest22@users.noreply.github.com>
2021-06-19 11:17:57 +0200

    Added a french version / Rajout d'une version FR

---
+++ Pendu.py
@@ -0,0 +1,54 @@
+#Cette fois-ci c'est un pendu (en français)
+
+#les imports
+import time
+import random
+import sys
+#voilà
+
+#les variables
+words_to_find = open("mot.txt").read().splitlines()
+word_to_find = random.choice(words_to_find).lower()
+# word_to_find = 
+#fin des variables
+
+#explication
+print ("Bonjour, on va faire un pendu .\nLe mot à trouver est sélectionné dans le dictionnaire des mots français et contient des prénoms courants\nTu peux deviner 7 lettres qui ne sont pas dans le mot avant de perdre.\nBonne chance !")
+time.sleep(1)
+#début du code
+banana_word = list(word_to_find)
+longueur_du_mot = len(word_to_find)
+guessing=longueur_du_mot * "_"
+listedudevinnage = list(guessing)
+banana_word_copy = banana_word.copy()
+guesses=7
+while banana_word_copy != listedudevinnage:
+    lettre = input("Propose une lettre\n")
+    nbrdefoislalettre = banana_word_copy.count(lettre)
+    nbrdefoislalettrepourleif = nbrdefoislalettre
+    for letter in banana_word:
+        if letter == lettre:
+            while nbrdefoislalettre != 0:
+                nbrdefoislalettre = nbrdefoislalettre - 1
+                lindex = banana_word.index(letter)
+                banana_word.pop(lindex)
+                banana_word.insert(lindex,"_")
+                listedudevinnage.pop(lindex)
+                listedudevinnage.insert(lindex, letter)
+        if nbrdefoislalettrepourleif == 0:
+            guesses=guesses-1
+            if guesses == 0:
+                print("Désolé ! Tu as perdu. Le mot était " + word_to_find + ".")
+                time.sleep(4)
+                sys.exit()
+            if guesses == 1:
+                print("Non, désolé. La lettre " + lettre + " n'est pas dans le mot.\nIl te reste " + str(guesses) + " essai avant de perdre")
+                break
+            else:
+                print("Non, désolé. La lettre " + lettre + " n'est pas dans le mot.\nIl te reste " + str(guesses) + " essais avant de perdre")
+                break
+    print (" ".join(listedudevinnage) + "\n")       
+else:
+    time.sleep(1)
+    print("Bien joué ! Tu as deviné le mot/nom qui était " + word_to_find)
+    time.sleep(4)
